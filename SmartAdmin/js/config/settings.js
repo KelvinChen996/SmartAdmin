e02e3337e7298e0f0dc7a18ea143e0fb4091fcc5
Seya Obey <seyaobey@gmail.com>
2016-06-14 14:54:16 +0300

    delete *.js, *.map files

--- SmartAdmin/js/config/settings.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../lib/jx.tsx" />
-define(["require", "exports"], function (require, exports) {
-    exports.settings = {
-        rootpage: '#content',
-        masterpage_template: '/html/masterpage.html'
-    };
-});
-//# sourceMappingURL=C:/StampDev/SmartAdmin/SmartAdmin/js/config/settings.js.map
